62fee5f511837c12d5f72e3a53c4354adc4df6ab
afeiship <1290657123@qq.com>
2017-04-19 08:56:01 +0800

    add static_pages / resource:m\ost/users

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require_relative 'boot'
 
 require 'rails/all'
 
+## for bootstrap:
+require File.expand_path('../boot', __FILE__)
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,5 +15,8 @@ module RailsPracticeWeibo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  # match '/', to: 'static_pages#home', via: 'get'
+  root to: 'static_pages#home'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
