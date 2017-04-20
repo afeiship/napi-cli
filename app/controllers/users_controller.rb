3dd429796d4e97bb329d3a43f1e16d9ee46ffc0a
afeiship <1290657123@qq.com>
2017-04-20 09:57:28 +0800

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { 
+          flash[:success] = "Welcome to the Sample App!"
+          redirect_to @user 
+        }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -42,7 +45,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, success: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -56,7 +59,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to users_url, success: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -70,6 +73,7 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       # params.require(:user).permit(:name, :email)
-      params.require(:user).permit!
+      # params.require(:user).permit!
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  # match '/', to: 'static_pages#home', via: 'get'
+  ## match '/', to: 'static_pages#home', via: 'get'
   root to: 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
@@ -9,7 +9,13 @@ Rails.application.routes.draw do
   ## register user:
   match '/signup', to: 'users#new', via: 'get'
 
+  ## login/logout:
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+
+  ## reources:
   resources :microposts
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
