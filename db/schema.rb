cbd9321b459a21b6f8687047e5d21e06758c937a
afeiship <1290657123@qq.com>
2017-04-26 00:02:25 +0800

    update micropost model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425141414) do
+ActiveRecord::Schema.define(version: 20170425150613) do
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   end
 
   create_table "users", force: :cascade do |t|
