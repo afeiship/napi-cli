051f056799e00135b912d50ec0e16c771bfd9d6a
afeiship <1290657123@qq.com>
2024-12-30 22:28:10 +0800

    feat: compress jpg

--- bin/index.js
+++ bin/index.js
@@ -2,17 +2,22 @@
 
 import { Command, Option } from 'commander';
 import { createRequire } from 'module';
+import fs from 'node:fs';
+import { Transformer, compressJpeg } from '@napi-rs/image';
 
 const __dirname = new URL('../', import.meta.url).pathname;
 const require = createRequire(__dirname);
 const pkg = require('./package.json');
 const program = new Command();
+const FORMAT_HOOKS = {
+  jpeg: 'jpg',
+  jpg: 'jpg',
+};
 
 program.version(pkg.version);
 program
   .addOption(new Option('-v, --verbose', 'show verbose log'))
   .addOption(new Option('-r, --replace', 'Replace existing file'))
-  .addOption(new Option('-s, --source <string>', 'Source file'))
   .addOption(new Option('-t, --target <string>', 'Target file'))
   .addOption(
     new Option('-f, --format <string>', 'Target file format').choices([
@@ -42,8 +47,24 @@ class CliApp {
     if (verbose) console.log('📗', ...args);
   }
 
-  run() {
-    this.log('run cli: ', __dirname, this.args, this.opts, pkg.version);
+  async run() {
+    const source = this.args[0];
+    if (!source) throw new Error('Source file is required');
+    const { target, format } = this.opts;
+    const fileBuffer = fs.readFileSync(source);
+    const suffix = source.split('.').pop();
+    const distBuffer = await this.compress(fileBuffer, suffix, { quality: 10 });
+    console.log('dist buffer: ', distBuffer);
+    fs.writeFileSync(target, distBuffer);
+  }
+
+  /* compress format */
+  compress(buffer, opts) {
+    const { format } = this.opts;
+    if (format === 'jpg' || format === 'jpeg') {
+      return compressJpeg(buffer, opts);
+    }
+    return new Transformer(buffer)[format](opts);
   }
 }
 
